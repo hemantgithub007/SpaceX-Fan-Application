d9a57fc7b897c63371b0a5464413828fca66857f
Hemant Kumar Nishad <hemant.nishad@teamcomputers.com>
2022-09-23 22:16:13 +0530

    Code setup on github

---
+++ SpaceX Fan Application/SpaceX Fan Application/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpaceX Fan Application
+//
+//  Created by teamcomputers on 23/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SpaceX Fan Application/SpaceX Fan ApplicationUITests/SpaceX_Fan_ApplicationUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SpaceX_Fan_ApplicationUITestsLaunchTests.swift
+//  SpaceX Fan ApplicationUITests
+//
+//  Created by teamcomputers on 23/09/22.
+//
+
+import XCTest
+
+class SpaceX_Fan_ApplicationUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
